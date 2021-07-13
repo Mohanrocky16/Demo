f5063131bf57bdb1e9866d10e00a7320a57d87bd
Mohanrocky16 <mohanvenugopal1694@gmail.com>
2021-07-13 21:18:59 +0530

    New Update

---
+++ src/test/java/org/data/Sample.java
@@ -0,0 +1,16 @@
+package org.data;
+
+public class Sample {
+	
+	public void projectOne() {
+		System.out.println("Project 1");
+
+	} 
+	
+	public void projectTwo() {
+		System.out.println("Project 2");
+
+	} 
+	
+
+}
